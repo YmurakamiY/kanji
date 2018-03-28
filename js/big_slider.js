773081f53ea48d2c05ac6cd3420861a4da52b70a
YmurakamiY <yu.murakami.seraku@gmail.com>
2018-03-28 14:38:58 +0900

    start

---
+++ js/big_slider.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+//メインスライド用
+  $('#big_slider').bxSlider({
+    auto: true,
+    mode: 'fade',
+    minSlides: 3,
+    controls: false
+  });
+});
+
+$(document).ready(function(){
+  //メインスライド用
+    $('#slider_img').bxSlider({
+      auto: true,
+      infiniteLoop: false,
+      hideControlOnEnd: true,
+      autoHover: true,
+      nextSelector: '#next-btn',
+      prevSelector: '#prev-btn',
+      nextText: '',
+      prevText: '',
+      pager: false
+    });
+  });
